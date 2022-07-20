9b90119571b4280b9c794057040099b97b68193a
NiharBabu <nihar.devathi@smarsh.com>
2022-07-20 18:30:28 +0530

    SAAS-21656 ci-pipeline changes

---
+++ ci/tasks/add_blob/task.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euox
+
+git config --global user.name "Concourse CI Bot"
+git config --global user.email "ci@localhost"
+
+pushd zookeepeer_bosh_release
+  bosh -n add-blob ../blob_dir/$BLOB_FILE $BLOB_PATH
+  bosh -n upload-blobs
+
+  git add config/blobs.yml
+  git commit -m "Update $BLOB_PATH blob"
+popd
+
+git clone zookeepeer_bosh_release updated_zookeepeer_bosh_release
