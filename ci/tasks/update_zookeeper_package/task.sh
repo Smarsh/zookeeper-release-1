502f219cf0f87948ea36df5ffcbcfef8b0591bfe
NiharBabu <nihar.devathi@smarsh.com>
2022-07-21 02:24:21 +0530

    SAAS-21656 ci-pipeline changes-add-blob

--- ci/tasks/update_zookeeper_package/task.sh
+++ ci/tasks/update_zookeeper_package/task.sh
@@ -2,6 +2,12 @@
 
 set -euox
 # download the apache zookeeper release. Currently this is hardcoded.
-wget https://dlcdn.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz
+#wget https://dlcdn.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz
 
-mv apache-zookeeper-3.8.0-bin.tar.gz zookeeper_package/zookeeper.tar.gz
+
+wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.10/zookeeper-3.4.10.tar.gz
+
+tar -xzvf zookeeper-3.4.10.tar.gz
+mv zookeeper-3.4.10 zookeeper
+tar -cvzf zookeeper.tar.gz zookeepeer
+mv zookeeper.tar.gz zookeeper_package/zookeeper.tar.gz
